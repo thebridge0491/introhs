3310575b374b04ef702505920cb1bc6f6c18d9ee
thebridge0491 <thebridge0491-codelab@yahoo.com>
2015-08-07 18:30:04 -0400

    Expand practice library -- classic functions & sequence ops

--- api/tests/ClassicProp.hs
+++ api/tests/ClassicProp.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module ClassicProp (tprops) where
 
+import Data.List
 import Data.Word
+import qualified Control.Monad.Writer as Writer
 
 import Test.Framework.Providers.QuickCheck2
 import Test.QuickCheck
@@ -12,23 +14,145 @@ import Data.Introhs.Practice.Classic
 
 epsilon = 0.001
 
+propSquare :: Int -> Property
+propSquare n = (n >= 1) ==>
+    foldl (\a f -> a && Util.inEpsilon (epsilon * ans) ans (f $ realToFrac n)) True
+    [squareR, squareI]
+  where ans = realToFrac n ** 2.0
+
+--propExpt :: (Eq a, Show a, Num a) => a -> a -> Bool
+propExpt :: Float -> Float -> Bool
+propExpt b n = foldl (\a f -> a && (Util.inEpsilon (epsilon * ans) ans (f b n))) 
+    True [exptR, exptI, fastExptR, fastExptI]
+  where ans = b ** n
+
+propSumTo :: Int -> Int -> Bool
+propSumTo hi lo = foldl (\a f -> a && f hi lo == ans) True
+    [sumToR, sumToI]
+  where ans = foldl (+) lo [(lo + 1)..hi]
+
 --propFact :: (Eq a, Show a, Ord a, Num a, Enum a) => a -> Bool
 propFact :: Word32 -> Bool
 propFact n = foldl (\a f -> a && f n == ans) True [factR, factI]
   where ans = foldl (*) 1 [1..n]
 
---propExpt :: (Eq a, Show a, Num a) => a -> a -> Bool
-propExpt :: Float -> Float -> Bool
-propExpt b n = foldl (\a f -> a && (Util.inEpsilon (epsilon * ans) ans (f b n))) 
-    True [exptR, exptI]
-  where ans = b ** n
+propFib :: Int -> Bool
+propFib n = foldl (\a f -> a && f n == ans) True 
+    [fibR, fibI] 
+  where ans = snd $ foldl (\(s0, s1) _ -> (s0 + s1, s0)) (0, 1) [0..n]
+
+propPascaltri :: Int -> Bool
+propPascaltri rows = foldl (\a f -> a && validNumRows (f rows) &&  
+        fst (foldl (\(a1, n) r -> (a1 && validLenRow n r && validSumRow n r, 
+        n + 1)) (True, 0) (f rows))) True
+        [pascaltriMult, pascaltriAdd]
+  where validNumRows res = length res == rows + 1
+        validLenRow n r = length r == n + 1
+        validSumRow n r = sum r == truncate (2 ** realToFrac n)
+
+propQuotRem :: Int -> Int -> Property
+propQuotRem m n = (n /= 0) ==> foldl (\a (fQ, fR) -> a && fQ m n == ansQ 
+    && fR m n == ansR) True
+    [(quotM, remM)]
+  where (ansQ, ansR) = quotRem m n
+
+propDivMod :: Int -> Int -> Property
+propDivMod m n = (n /= 0) ==> foldl (\a (fD, fM) -> a && fD m n == ansD
+    && fM m n == ansM) True
+    [(divM, modM)]
+  where (ansD, ansM) = divMod m n
+
+propGcdLcm :: [Int] -> Property
+propGcdLcm xss = case xss of
+    x:xs -> (length (x:xs) < 20) ==> 
+        foldl (\a (fG, fL) -> a && fG (x:xs) == ansG && fL (x:xs) == ansL
+        ) True
+        [(gcdR, lcmR), (gcdI, lcmI)]
+      where (ansG, ansL) = (foldl gcd (abs x) xs, foldl lcm (abs x) xs)
+    _ -> True ==> True
+
+propBaseExpand :: Int -> Int -> Bool
+propBaseExpand b n = foldl (\a f -> a && f b n == ans) True 
+    [baseExpandR, baseExpandI]
+  where ans = reverse $ unfoldr (\x -> if x == 0 then Nothing
+            else Just (mod x b, div x b)) n
+
+propBaseTo10 :: Int -> [Int] -> Bool
+propBaseTo10 b lst = foldl (\a f -> a && f b lst == ans) True 
+    [baseTo10R, baseTo10I]
+  where ans = snd $ foldr (\e (h, t) -> 
+            (h + 1, t + (e * truncate (fromIntegral b ** fromIntegral h))))
+            (0, 0) lst
+
+propRange :: Int -> Int -> Bool
+propRange hi lo = foldl (\a (f, fStep) -> a && f lo hi == ans 
+    && fStep 1 lo hi == ans) True
+    [(rangeR, rangeStepR), (rangeI, rangeStepI)]
+  where ans = [lo..hi]
+
+propComposeInt :: (Eq c, Show c, Integral c) => (b -> c) -> (a -> b) -> a -> Bool
+propComposeInt f g x = foldl (\a comp -> a && ans == comp f g x)
+    True [compose1]
+  where ans = (f . g) x
+
+propComposeFlt :: (Eq c, Show c, Ord c, Floating c) => (b -> c) -> (a -> b) -> a -> Bool
+propComposeFlt f g x = 
+    foldl (\a comp -> a && Util.inEpsilon (realToFrac epsilon * ans) ans (comp f g x))
+    True [compose1]
+  where ans = (f . g) x
+
+
+--propFactLog :: (Eq a, Show a, Num a) => Integer -> Bool
+propFactLog :: Integer -> Bool
+propFactLog n = foldl (\a f -> do
+    let (numFact, logFactI) = Writer.runWriter $ f n
+    a && numFact == ans && foldl (\a1 (_, msg) -> a1 && not (null msg))
+        True logFactI) True
+    [factILog]
+  where ans = foldl (*) 1 [1..(toInteger n)]
+
     
 -- Data.Functor.<$> ; GHC.Base.<*>   -- prefix fmap --> infix <$>
 genTup2Int = (,) <$> (elements ([1..20] :: [Float])) <*>
     (elements ([2..10] :: [Float]))
+genTup2 xs ys = (,) <$> elements xs <*> elements ys
+
+--propBase2Expand = propBaseExpand 2
+--propBase11Expand = propBaseExpand 11
+
+propBase2To10 lst = (length lst < 20) ==> propBaseTo10 2 lst
+propBase16To10 lst = (length lst < 10) ==> propBaseTo10 16 lst
+
+propSquareSqrt = propComposeFlt (** 2.0) sqrt
+propLengthRange = propComposeInt length (\n -> [0..n])
+
+--propComb1 = forAll (genTup2 ([1..20] :: [Float]) ([2..10] :: [Float])) . uncurry
 
 
 tprops = map (uncurry testProperty) $ 
-    [("fact n == List.product [1..n]", forAll (elements [0..18]) propFact)
+    [("square n == n ** 2.0", property propSquare)
     , ("expt b n == b ** n", (forAll genTup2Int . uncurry) propExpt)
+    , ("sumTo hi lo == List.sum [lo..hi]", property propSumTo)
+    , ("fact n == List.product [1..n]", forAll (elements [0..18]) propFact)
+    , ("fib n == ?? n", forAll (elements [0..20]) propFib)
+    , ("pascaltri rows == ?? rows", forAll (elements [0..15]) propPascaltri)
+    , ("(quotM m n, remM m n) == quotRem m n", property propQuotRem)
+    , ("(divM m n, modM m n) == divMod m n", property propDivMod)
+    , ("[gcd | lcm] m n == [gcd | lcm] m n"
+        , forAll (listOf1 $ elements [(-500)..500]) propGcdLcm)
+    , ("base2Expand n == ?? 2 n"
+        , forAll (elements [0..150]) (propBaseExpand 2))
+    , ("base11Expand n == ?? 11 n"
+        , forAll (elements [0..250]) (propBaseExpand 11))
+    , ("base2To10 digits == ?? 2 digits"
+        , forAll (listOf1 $ elements [0..1]) propBase2To10)
+    , ("base16To10 digits == ?? 16 digits"
+        , forAll (listOf1 $ elements [0..15]) propBase16To10)
+    , ("range hi lo == [lo..hi]", property propRange)
+    , ("composeLenRange == len . range"
+        , forAll (elements [0..20]) propLengthRange)
+    , ("composeSquareSqrt == square . sqrt"
+        , forAll (elements [0.0..100.0]) propSquareSqrt)
+    , ("factLog n == List.product [1..n]"
+        , forAll (elements [0..18]) propFactLog)
     ]
--- api/tests/SequenceopsProp.hs
+++ api/tests/SequenceopsProp.hs
@@ -3,6 +3,7 @@ module SequenceopsProp (tprops) where
 
 import Data.List
 import Data.Word
+import qualified Control.Monad.Writer as Writer
 
 import Test.Framework.Providers.QuickCheck2
 import Test.QuickCheck
@@ -13,22 +14,236 @@ import Data.Introhs.Practice.Sequenceops
 
 --epsilon = 0.001
 
+propTabulate :: Int -> Property
+propTabulate n = (n > 0 && n <= 10) ==> 
+    foldl (\a f -> a && f (\e -> e) n == ans && f (\e -> e + 2) n == ans2) True
+    [tabulateR, tabulateI]
+  where ans = reverse $ foldl (\a e -> e : a) [] [0..(n - 1)]
+        ans2 = reverse $ foldl (\a e -> e : a) [] [2..(n - 1 + 2)]
+
+propLength :: (Eq a, Show a) => [a] -> Bool
+propLength xs = foldl (\a f -> a && f xs == ans) True 
+    [lengthR, lengthI]
+  where ans = length xs
+
+propNth :: (Eq a, Show a) => Int -> [a] -> Property
+propNth idx xs = (idx >= 0 && idx < length xs) ==> 
+    foldl (\a f -> a && f idx xs == Just ans) True 
+    [nthR, nthI]
+  where ans = xs !! idx
+
+propFindIndex :: (Eq a, Show a) => (a -> Bool) -> [a] -> Bool
+propFindIndex pred1 xs = foldl (\a f -> a && f pred1 xs == ans) True 
+    [findIndexR, findIndexI]
+  where ans = findIndex pred1 xs
+
+propFind pred1 xs = foldl (\a f -> a && f pred1 xs == ans) True 
+    [findR, findI]
+  where ans = find pred1 xs
+
+propMinMax :: (Eq a, Show a, Ord a) => [a] -> Property
+propMinMax xs = (not . null) xs ==> 
+    foldl (\a (fMin, fMax) -> a && fMin xs == ansMin 
+        && fMax xs == ansMax) True
+        [(minR, maxR), (minI, maxI)]
+  where (ansMin, ansMax) = (minimum xs, maximum xs)
+
 propReverse :: (Eq a, Show a) => [a] -> Bool
 propReverse xs = foldl (\a f -> a && f xs == ans) True 
     [reverseR, reverseI]
   where ans = reverse xs
 
-propFindIndex :: (Eq a, Show a) => (a -> Bool) -> [a] -> Bool
-propFindIndex pred1 xs = foldl (\a f -> a && f 0 pred1 xs == ans) True 
-    [findIndexR, findIndexI]
-  where ans = findIndex pred1 xs
+propCopy :: (Eq a, Show a) => [a] -> Bool
+propCopy xs = foldl (\a f -> a && f xs == ans) True 
+    [copyR, copyI]
+  where ans = map id xs
+
+propTakeDrop :: (Eq a, Show a) => Int -> [a] -> Property
+propTakeDrop n xs = (n >= 0 && n < 50) ==> foldl (\a (fT, fD) -> a && 
+    fT n xs == ansT && fD n xs == ansD) True 
+    [(takeI, dropI)]
+  where (ansT, ansD) = (take n xs, drop n xs)
+
+propAnyAll :: (Eq a, Show a) => (a -> Bool) -> [a] -> Property
+propAnyAll pred1 xs = (length xs < 50) ==> foldl (\a (fAny, fAll) -> a && 
+    fAny pred1 xs == ansAny && fAll pred1 xs == ansAll) True 
+    [(anyR, allR), (anyI, allI)]
+  where (ansAny, ansAll) = (any pred1 xs, all pred1 xs)
+
+propMap :: (Eq b, Show b) => (a -> b) -> [a] -> Bool
+propMap proc xs = foldl (\a f -> a && f proc xs == ans) True 
+    [mapR, mapI]
+  where ans = map proc xs
+
+propFilterRemove :: (Eq a, Show a) => (a -> Bool) -> [a] -> Bool
+propFilterRemove pred1 xs = foldl (\a (fFil, fRem) -> a && 
+    fFil pred1 xs == ansFil && fRem pred1 xs == ansRem) True 
+    [(filterR, removeR), (filterI, removeI)]
+  where (ansFil, ansRem) = (filter pred1 xs, snd $ partition pred1 xs)
+
+propFoldl :: (Eq b, Show b) => (b -> a -> b) -> b -> [a] -> Property
+propFoldl corp init xs = (length xs < 50) ==> 
+    foldl (\a f -> a && f corp init xs == ans) True 
+    [foldlR, foldlI]
+  where ans = foldl corp init xs
+
+propFoldr :: (Eq b, Show b) => (a -> b -> b) -> b -> [a] -> Property
+propFoldr proc init xs = (length xs < 50) ==> 
+    foldl (\a f -> a && f proc init xs == ans) True 
+    [foldrR, foldrI]
+  where ans = foldr proc init xs
+
+propUnfoldleft :: (Eq a, Eq b, Show a, Show b) => (b -> Maybe (a, b)) -> b -> Bool
+propUnfoldleft func seed = foldl (\a f -> a && f func seed == ans) True 
+    [(unfoldleftR)]
+  where ans = unfoldr func seed
+
+propUnfoldright :: (Eq a, Eq b, Show a, Show b) => (b -> Maybe (a, b)) -> b -> Bool
+propUnfoldright func seed = foldl (\a f -> a && f func seed == ans) True 
+    [(unfoldrightI)]
+  where ans = reverse $ unfoldr func seed
+
+propIsOrdered :: (Ord a, Show a) => [a] -> Property
+propIsOrdered xs = (length xs < 25) ==> 
+    foldl (\a f -> a && f xs False == ans && f xs True == ans2) True 
+    [isOrderedR, isOrderedI]
+  where verifyfn _ [] = True
+        verifyfn cmpfn (y:ys) = 
+            fst $ foldl (\(a, cur) e -> ((cmpfn cur e) && a, e)) (True, y) ys
+        ans = (verifyfn (<=) xs)
+        ans2 = (verifyfn (>=) xs)
+
+propAppend :: (Eq a, Show a) => [a] -> [a] -> Bool
+propAppend xs ys = foldl (\a f -> a && f xs ys == ans) True 
+    [appendR, appendI]
+  where ans = xs ++ ys
+
+propInterleave :: (Eq a, Show a) => [a] -> [a] -> Property
+propInterleave xss yss = (length xss < 50) ==> (length yss < 50) ==>
+    foldl (\a f -> a && f xss yss == ans) True
+    [interleaveR, interleaveI]
+  where ans 
+            | length xss > length yss = 
+                concat (zipWith (\x y -> [x, y]) xss yss) ++ 
+                drop (length yss) xss
+            | otherwise = concat (zipWith (\x y -> [x, y]) xss yss) ++ 
+                drop (length xss) yss
+
+propMap2 :: (Eq c, Show c) => (a -> b -> c) -> [a] -> [b] -> Property
+propMap2 proc xs ys = (length xs < 25) ==> (length ys < 25) ==> 
+    foldl (\a f -> a && f proc xs ys == ans) True 
+    [map2R, map2I]
+  where ans = zipWith proc xs ys
+
+propZip :: (Eq a, Eq b, Show a, Show b) => [a] -> [b] -> Property
+propZip xs ys = (length xs < 25) ==> (length ys < 25) ==> 
+    foldl (\a f -> a && f xs ys == ans) True 
+    [zipR, zipI, zipM]
+  where ans = zip xs ys
+
+propUnzip2 :: (Eq a, Eq b, Show a, Show b) => [(a, b)] -> Property
+propUnzip2 xs = (length xs < 25) ==> 
+    foldl (\a f -> a && f xs == ans) True 
+    [unzip2I]
+  where ans = unzip xs
+
+propConcat :: (Eq a, Show a) => [[a]] -> Property
+propConcat nlsts = (length nlsts < 20) ==> 
+    foldl (\a f -> a && f nlsts == ans) True 
+    [concatR, concatI]
+  where ans = concat nlsts
+
+
+propReverseLog :: (Eq a, Show a) => [a] -> Bool
+propReverseLog xs = foldl (\a f -> do
+    let (lstRev, logReverseI) = Writer.runWriter $ f xs
+    a && lstRev == ans && foldl (\a1 (_, msg) -> a1 && not (null msg))
+        True logReverseI) True
+    [reverseILog]
+  where ans = reverse xs
+
 
 -- Data.Functor.<$> ; GHC.Base.<*>   -- prefix fmap --> infix <$>
+genTup2 xs ys = (,) <$> elements xs <*> elements ys
 --genTup2Int = (,) <$> (elements ([1..20] :: [Float])) <*>
 --    (elements ([2..10] :: [Float]))
 
+--propFindIndexEven = propFindIndex even :: [Int] -> Bool
+--propFind3 = propFind (== 3) :: [Int] -> Bool
+--propFilterRemoveEven = propFilterRemove even :: [Int] -> Bool
+--propAnyAllEven = propAnyAll even :: [Int] -> Property
+--propFoldlAdd = propFoldl (+) :: Int -> [Int] -> Property
+--propFoldrSub = propFoldr (-) :: Int -> [Int] -> Property
+
+--propMapPlus2 = propMap (+2) :: [Int] -> Bool
+--propMap2Plus2 = propMap2 (\e1 e2 -> (e1 + e2) + 2) :: [Int] -> [Int] -> Property
+
+propUnfoldrightRange n = propUnfoldright funcRange (0, n)
+  where funcRange (lo, hi) = if lo > hi then Nothing 
+            else Just (lo, (lo + 1, hi))
+propUnfoldleftFib n = propUnfoldleft funcFib (0, 1, n)
+  where funcFib (s0, s1, num) = if num < 0 then Nothing 
+            else Just (s0, (s0 + s1, s0, num - 1))
+propUnfoldrightBaseExpand b = propUnfoldright funcBsExpand
+  where funcBsExpand num = if num <= 0 then Nothing 
+            else Just (mod num b, div num b)
+propUnfoldleftUnsum n = propUnfoldleft funcUnfoldSum (0, n)
+  where funcUnfoldSum (start, stop) = if start > stop then Nothing 
+            else Just (start, (start + 1, stop - start))
+propUnfoldrightUnprod n = propUnfoldright funcUnfoldProd (1, n)
+  where funcUnfoldProd (start, stop) = if start > stop then Nothing 
+            --else Just (start, (start + 1, stop / start))
+            else Just (start, (start * stop, stop - 1))
+
+propComb0 = forAll (elements $ map (\n -> sum [0..n]) [0..25])
+propComb1 = forAll (elements $ map (\n -> product [1..n]) [1..17])
+propComb2 = forAll (genTup2 [2..16] [0..1024]) . uncurry
+
 
 tprops = map (uncurry testProperty) $ 
-    [("reverse == List.reverse", property (propReverse :: [Int] -> Bool))
-    , ("findIndex == List.findIndex", property (propFindIndex even :: [Int] -> Bool))
-    ]
+    [("tabulate == reverse $ foldl (\a e -> e : a) [] [0..(n - 1)]"
+        , property (propTabulate :: Int -> Property))
+    , ("length == List.length", property (propLength :: [Int] -> Bool))
+    , ("nth == List.!!", property (propNth :: Int -> [Int] -> Property))
+    , ("findIndex even == List.findIndex", property (propFindIndex even :: [Int] -> Bool))
+    , ("find (== 3) == List.find (== 3)", property (propFind (== 3) :: [Int] -> Bool))
+    , ("minMax == List.[min|max]imum"
+        , property (propMinMax :: [Int] -> Property))
+    , ("reverse == List.reverse", property (propReverse :: [Int] -> Bool))
+    , ("copy == List.map id", property (propCopy :: [Int] -> Bool))
+    , ("[take|drop] n == List.[take|drop] n"
+        , property (propTakeDrop :: Int -> [Int] -> Property))
+    , ("[any|all] even == List.[any|all] even", property (propAnyAll even :: [Int] -> Property))
+    , ("map (+2) == List.map (+2)", property (propMap (+2) :: [Int] -> Bool))
+    , ("[filter|remove] even == List.[filter|partition] even"
+        , property (propFilterRemove even :: [Int] -> Bool))
+    , ("foldl (+) == List.foldl (+)", property (propFoldl (+) :: Int -> [Int] -> Property))
+    , ("foldr (-) == List.foldr (-)", property (propFoldr (-) :: Int -> [Int] -> Property))
+    , ("unfoldright funcRange (0, n) == List.unfoldr funcRange (0, n)"
+        , forAll (elements [0..25]) propUnfoldrightRange)
+    , ("unfoldleft funcFib (0, 1, n) == reverse $ List.unfoldr funcFib (0, 1, n)"
+        , forAll (elements [0..25]) propUnfoldleftFib)
+    , ("unfoldright funcBsExpand (b, n) == List.unfoldr funcBsExpand (b, n)"
+        , propComb2 propUnfoldrightBaseExpand)
+    , ("unfoldleft funcUnfoldSum (0, n) == reverse $ List.unfoldr funcUnfoldSum (0, n)"
+        , propComb0 propUnfoldleftUnsum)
+    , ("unfoldright funcUnfoldProd (1, n) == List.unfoldr funcUnfoldProd (1, n)"
+        , propComb1 propUnfoldrightUnprod)
+    , ("isOrderedInts == fst $ foldl (\a e -> ((x <= e) && a, e)) True xs"
+        , property (propIsOrdered :: [Int] -> Property))
+    , ("isOrderedChars == fst $ foldl (\a e -> ((x <= e) && a, e)) True xs"
+        , property (propIsOrdered :: [Char] -> Property))
+    , ("append == List.++"
+        , property (propAppend :: [Int] -> [Int] -> Bool))
+    , ("interleave xs ys == ?? xs ys"
+        , property (propInterleave :: [Int] -> [Int] -> Property))
+    , ("map2 (e1 + e2 + 2) == List.zipWith (e1 + e2 +2)"
+        , property (propMap2 (\e1 e2 -> (e1 + e2) + 2) :: [Int] -> [Int] -> Property))
+    , ("zip == List.zip", property (propZip :: [Int] -> [Int] -> Property))
+    , ("unzip2 == List.unzip"
+        , property (propUnzip2 :: [(Int, Int)] -> Property))
+    , ("concat == List.concat", property (propConcat :: [[Int]] -> Property))
+    
+    , ("reverseLog == List.reverse"
+        , property (propReverseLog :: [Int] -> Bool))
+  ]
